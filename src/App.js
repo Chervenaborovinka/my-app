40af44bf0cbc5f41999edfe64dad2760107071da
Liza <yogurtmaniac@gmail.com>
2024-02-10 21:03:58 +0300

    npm google chart to the app

--- src/App.js
+++ src/App.js
@@ -1,24 +1,30 @@
 import logo from './logo.svg';
 import './App.css';
+import { TwitterPicker } from 'react-color';
+import { Chart } from "react-google-charts";
 
-function App() {
+export const data = [
+  ["Task", "Hours per Day"],
+  ["HTML", 11],
+  ["JS", 25],
+  ["CSS", 23],
+  ["Watch Lectures", 20],
+  ["Procrastination", 71],
+];
+
+export const options = {
+  title: "Моя учёба в ITGirl School",
+};
+
+export function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Chart
+      chartType="PieChart"
+      data={data}
+      options={options}
+      width={"100%"}
+      height={"400px"}
+    />
   );
 }
 
